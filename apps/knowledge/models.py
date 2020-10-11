36fb353720e78f1d5ac57527ad50f3b89e073dd4
Altynbek <go.gelleson@gmail.com>
2020-10-12 00:02:57 +0600

    add Scope model to knowledge app

--- apps/knowledge/models.py
+++ apps/knowledge/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Scope(models.Model):
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
