03fde00329e1cf66e9a235a14723328ce5983922
Altynbek <go.gelleson@gmail.com>
2020-10-12 00:45:08 +0600

    add urls.py to knowledge app and to global urls.py

---
+++ apps/knowledge/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import ScopeModelViewSet, NoteModelViewSet
+
+router = DefaultRouter()
+
+router.register("scopes/", ScopeModelViewSet)
+router.register("scopes/(?P<scope_id>[0-9]+)/notes/", NoteModelViewSet)
+router.register("notes", NoteModelViewSet)
+
+urlpatterns = router.urls
